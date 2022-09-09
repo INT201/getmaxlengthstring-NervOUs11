a1dd650cde3cba388db319a6552947a97b42b86e
Nitis <reberrus@gmail.com>
2022-09-09 20:41:56 +0700

    Test2

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -15,18 +15,13 @@ function getMaxLengthString(arrayOfString) {
   }
 
   for (let index = 0; index < arrayOfString.length; index++) {
-    if (arrayOfString[index].length == max){
-      result[index] = arrayOfString[index]
-    }    
+    if (arrayOfString[index].length == max) {
+      result.push(arrayOfString[index])
+    }   
   }
   return result
 }
 module.exports = getMaxLengthString
 
 
-// console.log(getMaxLengthString(['hello', 'hi', 'hey'])) //return ['hello']
-// console.log(getMaxLengthString(['hello', 'hi', 'hey', 'Good Evening', 'Good Morning'])) 
-//                                 //return ['Good Evening', 'Good Morning']
-// console.log(getMaxLengthString(['hi'])) //return ['hi']
-// console.log(getMaxLengthString(null)) //return undefined
-// console.log(getMaxLengthString(undefined)) //return undefined
+
